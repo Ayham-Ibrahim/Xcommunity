b27a3ee204c2273909fdb448efe9a25634e5f2f0
YousefSaleh1 <ys9882111@gmail.com>
2024-01-30 16:44:22 +0200

    Visitor System

---
+++ app/Http/Traits/VisitorableTrait.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Traits;
+
+use App\Models\User;
+use App\Models\Visitor;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+trait VisitorableTrait
+{
+    public function visitor(): MorphMany
+    {
+        return $this->morphMany(Visitor::class, 'visitorable');
+    }
+
+    public function visit(User $user)
+    {
+        if (!($this->isVisitor($user))) {
+            $this->addVisitor($user);
+        }
+    }
+
+    public function isVisitor(User $user): bool
+    {
+        return $this->visitor()->where('user_id', $user->id)
+            ->where('visitorable_id', $this->id)
+            ->where('visitorable_type', get_class($this))
+            ->exists();
+    }
+
+    private function addVisitor(User $user)
+    {
+        $user->visitor()->create([
+            'visitorable_id'   => $this->id,
+            'visitorable_type' => get_class($this),
+            'user_id'          => $user->id,
+        ]);
+    }
+
+
+    public function visitorCount(): int
+    {
+        return $this->visitor->count();
+    }
+}
